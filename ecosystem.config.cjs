269635cf2bd01dbe9f5ec0944cd5d723556b5d80
root <root@localhost.localdomain>
2026-06-17 22:11:39 +0700

    Cập nhật build version

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       script: "pm2_worker.js",
       interpreter: "node",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
